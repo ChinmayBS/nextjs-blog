870dbed806b3fbd938c2613d09d1485be344d357
Chinmay B S <66880207+ChinmayBS@users.noreply.github.com>
2021-12-02 01:51:34 +0530

    update img path

--- components/layout.js
+++ components/layout.js
@@ -31,7 +31,7 @@ export default function Layout({ children, home }) {
             <Image
               priority
 
-              src="/public/images/profile.jpg"
+              src="/images/profile.jpg"
 
               className={utilStyles.borderCircle}
               height={144}
@@ -46,7 +46,7 @@ export default function Layout({ children, home }) {
               <a>
                 <Image
                   priority
-                  src="/public/images/profile.jpg"
+                   src="/images/profile.jpg"
                   className={utilStyles.borderCircle}
                   height={108}
                   width={108}
